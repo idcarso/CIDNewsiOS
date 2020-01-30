6bc653f6d6d49f29eb95110d57eaff5b57155da7
Alejandro Jimenez <dncalexjimenez@outlook.com>
2020-01-30 14:07:24 -0600

    Adición de table view controller junto con clase table view controller para menu slide

---
+++ Cid/MenuSlideTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuSlideTableViewController.swift
+//  Cid
+//
+//  Created by Ada Palazuelos on 1/30/20.
+//  Copyright © 2020 Mac CTIN . All rights reserved.
+//
+
+import UIKit
+
+class MenuSlideTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
