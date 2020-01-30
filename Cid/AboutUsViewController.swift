42b79e8723104cd3e33a922ad12e84144bf03e84
Alejandro Jimenez <dncalexjimenez@outlook.com>
2020-01-30 11:55:45 -0600

    Adición de view controller junto con clase view controller para la pantalla de About Us

---
+++ Cid/AboutUsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutUsViewController.swift
+//  Cid
+//
+//  Created by Ada Palazuelos on 1/30/20.
+//  Copyright © 2020 Mac CTIN . All rights reserved.
+//
+
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
